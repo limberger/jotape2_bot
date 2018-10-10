e994e944ebc527c3693957b32e294e133734c969
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2018-10-09 23:25:49 -0300

    initiall

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ let bot;
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  console.log("Webhook setado " + process.env.HEROKU_URL + bot.token )
 }
 else {
   bot = new Bot(token, { polling: true });
